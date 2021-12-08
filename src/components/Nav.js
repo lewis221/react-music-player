165caa4d2918a53de05b42acded8b800b0686379
Lewis Chan <lewischan221@gmail.com>
2021-12-08 22:50:30 +0800

    little update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
-    return (
-        <nav>
-            <h1>Waves</h1>
-            <button onClick={() => setLibraryStatus(!libraryStatus)}>
-                Library
-                <FontAwesomeIcon icon={faMusic} />
-            </button>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <h1>Music Player</h1>
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        Library
+        <FontAwesomeIcon icon={faMusic} />
+      </button>
+    </nav>
+  );
+};
 
 export default Nav;
